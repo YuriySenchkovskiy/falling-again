7947df52a36f2f3d6e9ed23767f84c66d6a8a42c
Yuriy <yuriy.senchkovskiy@gmail.com>
2022-08-14 02:28:09 +0300

    Fix destroy object problem

--- Assets/Scripts/Based/DestroyComponent.cs
+++ Assets/Scripts/Based/DestroyComponent.cs
@@ -4,15 +4,19 @@ namespace Based
 {
     public class DestroyComponent : MonoBehaviour
     {
+        [SerializeField] private float _time;
+
+        private void Awake()
+        {
+            if (_time > 0)
+            {
+                Invoke(nameof(Destroy), _time);
+            }
+        }
+
         public void Destroy()
         {
             Destroy(gameObject);
-        }
-
-        public void DestroyAnotherGameObject(GameObject go)
-        {
-            Debug.Log("here");
-            Destroy(go);
         }
     }
 }
