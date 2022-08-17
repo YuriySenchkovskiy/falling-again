a641c56fb64bfa70f098adff121557fd75f63d88
Yuriy <yuriy.senchkovskiy@gmail.com>
2022-08-17 12:35:58 +0300

    Change gameplay

--- Assets/Scripts/GroundController/BlockController.cs
+++ Assets/Scripts/GroundController/BlockController.cs
@@ -1,17 +1,24 @@
+using System;
 using UnityEngine;
 
 namespace GroundController
 {
     public class BlockController : MonoBehaviour
     {
-        [SerializeField] private float _verticalSpeed;
+        [SerializeField] private float _startVerticalSpeed;
         [SerializeField] private Rigidbody2D _rigidbody;
-        
+
+        private void Start()
+        {
+            var velocity = new Vector2(_rigidbody.velocity.x, _startVerticalSpeed);
+            _rigidbody.velocity = velocity;
+        }
+
         private void FixedUpdate()
         {
-            if (_rigidbody.velocity.y < _verticalSpeed)
+            if (Mathf.Abs(_rigidbody.velocity.y) > Mathf.Abs(_startVerticalSpeed))
             {
-                var velocity = new Vector2(_rigidbody.velocity.x, _verticalSpeed);
+                var velocity = new Vector2(_rigidbody.velocity.x, _startVerticalSpeed);
                 _rigidbody.velocity = velocity;
             }
         }
